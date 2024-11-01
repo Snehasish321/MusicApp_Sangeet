844178161296fc849d153673ac4f3fa2f4e303dd
Snehasish Mondal <snehasishmondal01@gmail.com>
2024-11-02 00:43:12 +0530

    supabase_add

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,11 @@
 import Sidebar from '@/components/Sidebar';
 import './globals.css';
 import { Figtree } from 'next/font/google'
+import SupabaseProvider from '@/providers/SupabaseProvider';
+import UserProvider from '@/providers/UserProvider';
 
 const font = Figtree ({ subsets: ['latin']})
+
 
 export const metadata = {
   title: 'Sangeet',
@@ -17,9 +20,13 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <body className={font.className}>
+        <SupabaseProvider>
+          <UserProvider>
         <Sidebar>
         {children}
         </Sidebar>
+        </UserProvider>
+        </SupabaseProvider>
       </body>
     </html>
   )
